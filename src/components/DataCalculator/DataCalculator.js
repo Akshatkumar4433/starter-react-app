cdc5012d3cd3e543471f1088d96013040ed6dd98
Akshat Kumar <akshatkumar4433@gmail.com>
2022-11-19 07:04:50 -0500

    table update

--- src/components/DataCalculator/DataCalculator.js
+++ src/components/DataCalculator/DataCalculator.js
@@ -6,98 +6,101 @@ class DataCalculatorDashboard extends React.Component {
         cells: [{id:1,name:'$10', start:5,increments:0, end:0}],
     }
 
-    updateIncrements = (key, increment) => {
-        const cells = this.state.cells.map((cell) => { 
-                            if (cell.id === key) {
-                                cell.increments += Number(increment)
-                                return cell;
-                            }
-                            else {
-                                return cell
-                            }
-
+    updateIncrements = (id, increment) => {
+        const cells = this.state.cells.map(cell => {
+            if (cell.id === id) {
+                cell.increments += increment
+            }
+            return cell
         })
-        console.log(cells)
-
-
+        this.setState({cells:cells})
     }
     
     render() {
         return (
             <div>
-                        <RawColumn/>
-                        <IncrementColumn
-                            updateIncrements = {this.updateIncrements}
-                            id = {1}
-                        />
-                        <RawColumn/>
+                   <Row
+                        id = {this.state.cells[0].id}
+                        name = {this.state.cells[0].name}
+                        start = {this.state.cells[0].start}
+                        increments = {this.state.cells[0].increments}
+                        end = {this.state.cells[0].end}
+                        updateIncrements = {this.updateIncrements}
+                   />     
                     
             </div>
         );
     };
 };
 
-
-class IncrementColumn extends React.Component {
-     constructor(props) {
-         super(props);
-         this.state = {
-             //is this prop important?
-             increments:this.props.increments,
-             increment:0
-         }
-    }
-    handleSubmit = (event) => {
-        const key = this.props.id;
-        const increment = this.state.increment;
-        this.props.updateIncrements(key,increment)
-        event.preventDefault();
-    }
-    
-    handleInput = (event)=> {
-        this.setState({increment:event.target.value})
-    }
-
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        type = 'text'
-                        value = {this.state.increment}
-                        onChange = {this.handleInput}
-                    />
-                </form>    
-            </div>
-        )
-    }
-}
-
-class RawColumn extends React.Component {
+class Row extends React.Component {
+    //Make a constructor
     state = {
-        text: ''
-    }
-    handleInput = (event)=> {
-        this.setState({text:event.target.value})
+        incrementValue:0,
+        start:0,
+        end:0,
+        startDisabled:true,
+        endDisabled: true,
     }
 
-    handleSubmit = (event) => {
+    handleRow = (event) => {
+        this.props.updateIncrements(this.props.id,
+             this.state.incrementValue)
         event.preventDefault();
+        
+    }
+
+    handleValueChange = (event) => {
+         //event.target.name
+    }
+
+    handleDisable = (input) => {
+        return () => {
+            if (input === 'start') {
+                this.setState({startDisabled: !this.state.startDisabled})
+            }
+            else {
+                this.setState({endDisabled: !this.state.endDisabled})
+            }
+        
+        }
     }
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input 
-                        type='text'
-                        onChange={this.handleInput}
-                        value = {this.state.text}
+                <form onSubmit = {this.handleRow}>
+                    <h5>{this.props.name}</h5>
+                    <label
+                        onClick = {this.handleDisable('start')}
+                        >start:</label>
+                    <input type = 'text'
+                        name = 'start' 
+                        value = {this.props.start}
+                        disabled = {this.state.startDisabled}
+                        onChange = {this.handleValueChange}
                     />
+                    <label>increment:</label>
+                    <input type = 'text'
+                        name = 'increment'
+                        onChange={this.handleValueChange}
+                        value = {this.state.incrementValue}
+                    />
+                    <label
+                        onClick = {this.handleDisable('end')}
+                    >
+                        end:
+                    </label>
+                    <input type = 'text' 
+                        name = 'end'
+                        value = {this.props.end}
+                        disabled = {this.state.endDisabled}
+                        onChange = {this.handleValueChange}
+                    />
+                    <input type = 'submit' value = 'update'/>
                 </form>
             </div>
         );
-    }
+    };
 }
 
 
