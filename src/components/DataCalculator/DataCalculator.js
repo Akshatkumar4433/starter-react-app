d9a5aa310840dd9989c5a0f4e04f8ac10ad16ae8
Akshat Kumar <akshatkumar4433@gmail.com>
2022-11-22 23:17:26 -0500

    total calculated

--- src/components/DataCalculator/DataCalculator.js
+++ src/components/DataCalculator/DataCalculator.js
@@ -3,31 +3,55 @@ import React from "react";
 
 class DataCalculatorDashboard extends React.Component {
     state = {
-        cells: [{id:1,name:'$10', start:5,increments:0, end:0}],
+        cells: [
+            {id:1,name:'$3',price:3, start:76,increment:0, end:74},
+            {id:2,name:'$5', price:5, start:56,increment:0, end:50}
+        ],
+        total:false, //To hide it
     }
 
-    updateIncrements = (id, increment) => {
+    calculateTotal = () => {
+         let cells = this.state.cells;
+         let total = 0;
+         for(let i = 0; i<cells.length; i++) {
+             let ticketSold = cells[i].start + cells[i].increment - cells[i].end;
+             total += ticketSold * cells[i].price
+         }
+
+         this.setState({total:total})
+         
+    }
+
+    updateValues = (fields,id) => {
         const cells = this.state.cells.map(cell => {
             if (cell.id === id) {
-                cell.increments += increment
+                cell.start = fields.start;
+                cell.increment += fields.increment;
+                cell.end = fields.end;
             }
             return cell
         })
         this.setState({cells:cells})
+        
     }
-    
+
     render() {
         return (
             <div>
-                   <Row
-                        id = {this.state.cells[0].id}
-                        name = {this.state.cells[0].name}
-                        start = {this.state.cells[0].start}
-                        increments = {this.state.cells[0].increments}
-                        end = {this.state.cells[0].end}
-                        updateIncrements = {this.updateIncrements}
-                   />     
-                    
+                 {this.state.cells.map(cell => (
+                        <Row
+                            id = {cell.id}
+                            name = {cell.name}
+                            start = {cell.start}
+                            end = {cell.end}
+                            updateValues = {this.updateValues}
+                        />
+                 ))}
+
+                <div>
+                    <button onClick = {this.calculateTotal}>Total</button>
+                    {this.state.total}
+                </div>
             </div>
         );
     };
@@ -35,23 +59,34 @@ class DataCalculatorDashboard extends React.Component {
 
 class Row extends React.Component {
     //Make a constructor
-    state = {
-        incrementValue:0,
-        start:0,
-        end:0,
-        startDisabled:true,
-        endDisabled: true,
+    constructor(props) {
+        super(props)
+        this.state = {
+            fields: {
+                increment:0,
+                start:this.props.start,
+                end: this.props.end,
+            },
+        }
+
     }
+    
 
     handleRow = (event) => {
-        this.props.updateIncrements(this.props.id,
-             this.state.incrementValue)
+        //send start, incrementValue and end to 
+        //parent for update
+        this.props.updateValues(this.state.fields,this.props.id)
+        const fields = this.state.fields;
+        fields.increment = 0;
+        this.setState({fields:fields})
         event.preventDefault();
         
     }
 
     handleValueChange = (event) => {
-         //event.target.name
+         let fields = this.state.fields;
+         fields[event.target.name] = Number(event.target.value);
+         this.setState({fields:fields})
     }
 
     handleDisable = (input) => {
@@ -70,29 +105,21 @@ class Row extends React.Component {
             <div>
                 <form onSubmit = {this.handleRow}>
                     <h5>{this.props.name}</h5>
-                    <label
-                        onClick = {this.handleDisable('start')}
-                        >start:</label>
                     <input type = 'text'
                         name = 'start' 
-                        value = {this.props.start}
+                        value = {this.state.fields.start}
                         disabled = {this.state.startDisabled}
                         onChange = {this.handleValueChange}
                     />
-                    <label>increment:</label>
+                    
                     <input type = 'text'
                         name = 'increment'
                         onChange={this.handleValueChange}
-                        value = {this.state.incrementValue}
+                        value = {this.state.fields.incrementValue}
                     />
-                    <label
-                        onClick = {this.handleDisable('end')}
-                    >
-                        end:
-                    </label>
                     <input type = 'text' 
                         name = 'end'
-                        value = {this.props.end}
+                        value = {this.state.fields.end}
                         disabled = {this.state.endDisabled}
                         onChange = {this.handleValueChange}
                     />
